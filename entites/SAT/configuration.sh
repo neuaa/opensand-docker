052eaa248e8b81db65c3feedb0bf24bc1bbe5f4d
neuaa <martin.frisch@hotmail.fr>
2020-02-17 08:23:28 +0100

    un commit

--- entites/SAT/configuration.sh
+++ entites/SAT/configuration.sh
@@ -8,7 +8,7 @@ opensand_docker_path=./../..
 sudo cp $PWD/entites/SAT/daemonbck.conf $PWD/entites/SAT/daemonbck${simulation_id}.conf
 #sudo cp /home/neuaa/ProjetLong/simulation/opensand-docker/entites/SAT/daemonbck.conf /home/neuaa/ProjetLong/simulation/opensand-docker/entites/SAT/daemonbck${simulation_id}.conf
 
-sudo sed -i 's+$emu_ipv4+'${addr_ipv4}'/24+g' $PWD/entites/daemonbck${simulation_id}.conf
+sudo sed -i 's+$emu_ipv4+'${addr_ipv4}'/24+g' $PWD/entites/SAT/daemonbck${simulation_id}.conf
 #sudo sed -i 's+$emu_ipv4+'${addr_ipv4}'/24+g' /home/neuaa/ProjetLong/simulation/opensand-docker/entites/SAT/daemonbck${simulation_id}.conf
 
 docker create -it --privileged -v $PWD/entites/SAT/daemonbck${simulation_id}.conf:/etc/opensand/daemonbck.conf --net opensand-network-${simulation_id} --ip $addr_ipv4 --hostname sat-${simulation_id} --name sat-${simulation_id} opensand-daemon
