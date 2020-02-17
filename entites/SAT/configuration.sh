78ce5340c47bba3674160222129fffb1eedaae1a
neuaa <martin.frisch@hotmail.fr>
2020-02-17 08:11:01 +0100

    un commit

--- entites/SAT/configuration.sh
+++ entites/SAT/configuration.sh
@@ -5,7 +5,7 @@ addr_ipv4=192.168.1${simulation_id}.10
 opensand_docker_path=./../..
 
 
-sudo cp $PWD/entites/SAT/daemonbck.conf $PWD/entites/daemonbck${simulation_id}.conf
+sudo cp $PWD/entites/SAT/daemonbck.conf $PWD/entites/SAT/daemonbck${simulation_id}.conf
 #sudo cp /home/neuaa/ProjetLong/simulation/opensand-docker/entites/SAT/daemonbck.conf /home/neuaa/ProjetLong/simulation/opensand-docker/entites/SAT/daemonbck${simulation_id}.conf
 
 sudo sed -i 's+$emu_ipv4+'${addr_ipv4}'/24+g' $PWD/entites/daemonbck${simulation_id}.conf
